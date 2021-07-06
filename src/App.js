1e36582df705cb922c8cf4d4737045f8e6f3bd0a
Thalles Carneiro <thallescarneiro@gmail.com>
2021-07-06 15:59:52 -0300

    Fixação - Componente Link - Ex. 1 - 6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
+import About from './components/About';
+import Users from './components/Users';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <Router>
+        <nav>
+          <ul>
+            <li><Link to="/" > Home </Link></li>
+            <li><Link to="/users" > Users </Link></li>
+            <li><Link to="/about" > About </Link></li>
+          </ul>
+        </nav>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/users" component={Users} />
+      </Router>
     );
   }
 }
